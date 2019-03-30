ecb5f7a84267f836691c089daa93a349fbc4403c
swanandnabar <snabar@smu.edu>
2019-03-29 23:11:08 -0700

    Update calculator app

--- src/main/java/com/practice/Apps/Calculator.java
+++ src/main/java/com/practice/Apps/Calculator.java
@@ -5,72 +5,51 @@ import java.util.Scanner;
 public class Calculator {
 
     private static void calculate() {
-        System.out.println("Enter your operation");
-        System.out.println("add, sub, divide, multiply");
+        System.out.println("Enter two numbers ");
 
         Scanner reader = new Scanner(System.in);
-
-        String userSelects = reader.next();
-
-
-        if (userSelects.equals("add")) {
-            addTwoNumbers();
-        } else if (userSelects.equals("sub")) {
-            subTwoNumbers();
-        } else if (userSelects.equals("divide")) {
-            divideTwoNumbers();
-        } else if (userSelects.equals("multiply")) {
-            multiplyTwoNumbers();
-        } else {
-            calculate();
-        }
-    }
-
-
-    private static void addTwoNumbers() {
-        System.out.println("Enter two numbers to add ");
-        Scanner reader = new Scanner(System.in);
-
-        int n1 = reader.nextInt();
-        int n2 = reader.nextInt();
-
-        System.out.println("Sum of two numbers is " + (n1 + n2) + ".");
-        calculate();
-    }
-
-
-    private static void subTwoNumbers() {
-        System.out.println("Enter two numbers to subtract ");
-        Scanner reader = new Scanner(System.in);
-
-        int n1 = reader.nextInt();
-        int n2 = reader.nextInt();
-
-        System.out.println("Difference of two numbers is " + (n1 - n2) + ".");
-        calculate();
-    }
-
-
-    private static void divideTwoNumbers() {
-        System.out.println("Enter two numbers to divide ");
-        Scanner reader = new Scanner(System.in);
-
         double n1 = reader.nextDouble();
         double n2 = reader.nextDouble();
 
-        System.out.println("Division of two numbers is " + (n1 / n2) + ".");
+
+        System.out.println("Enter your operation");
+        System.out.println("add(a), subtract(s), divide(d), multiply(m)");
+        String userSelection = reader.next();
+
+        if (userSelection.equals("a")) {
+            addTwoNumbers(n1,n2);
+        } else if (userSelection.equals("s")) {
+            subTwoNumbers(n1,n2);
+        } else if (userSelection.equals("d")) {
+            divideTwoNumbers(n1,n2);
+        } else if (userSelection.equals("m")) {
+            multiplyTwoNumbers(n1,n2);
+        } else {
+            calculate();
+        }
+
+    }
+
+    private static void addTwoNumbers(double n1, double n2) {
+        System.out.println("Sum of two numbers is " + (n1 + n2));
         calculate();
     }
 
 
-    private static void multiplyTwoNumbers() {
-        System.out.println("Enter two numbers to multiply ");
-        Scanner reader = new Scanner(System.in);
+    private static void subTwoNumbers(double n1, double n2) {
+        System.out.println("Difference of two numbers is " + (n1 - n2));
+        calculate();
+    }
 
-        int n1 = reader.nextInt();
-        int n2 = reader.nextInt();
 
-        System.out.println("Multiplication of two numbers is " + (n1 * n2) + ".");
+    private static void divideTwoNumbers(double n1, double n2) {
+        System.out.println("Division of two numbers is " + (n1 / n2));
+        calculate();
+    }
+
+
+    private static void multiplyTwoNumbers(double n1, double n2) {
+        System.out.println("Multiplication of two numbers is " + (n1 * n2));
         calculate();
     }
 
