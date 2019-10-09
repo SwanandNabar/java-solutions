ff7aff22111f064da390897e4656271617477ceb
swanandnabar <snabar@smu.edu>
2019-10-09 11:55:55 -0700

    Find identical binary tree and return true.

---
+++ src/main/java/com/practice/LeetCode/SameTree.java
@@ -0,0 +1,58 @@
+package com.practice.LeetCode;
+
+public class SameTree {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node (int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    Node root1, root2;
+
+    boolean identicalTrees (Node a, Node b) {
+        /*
+        If both empty
+         */
+        if (a == null && b == null) {
+            return true;
+        }
+
+        /*
+        both non-empty then compare with recursion
+         */
+        if (a!= null && b!= null) {
+            return (a.data == b.data && identicalTrees(a.left, b.left) && identicalTrees(a.right, b.right));
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        SameTree tree = new SameTree();
+
+        tree.root1 = new Node(1);
+        tree.root1.left = new Node(2);
+        tree.root1.right = new Node(3);
+        tree.root1.left.left = new Node(4);
+        tree.root1.left.right = new Node(5);
+
+        tree.root2 = new Node(1);
+        tree.root2.left = new Node(2);
+        tree.root2.right = new Node(3);
+        tree.root2.left.left = new Node(4);
+        tree.root2.left.right = new Node(5);
+
+        if (tree.identicalTrees(tree.root1, tree.root2))
+            System.out.println("Both trees are identical");
+        else
+            System.out.println("Trees are not identical");
+
+    }
+
+
+}
